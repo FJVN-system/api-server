79c7e797dcdf625a25698f86e73c3a4bc678858a
KyungHwanKim-devs <kkhdevs@gmail.com>
2023-03-06 23:02:21 +0900

    주문 DTO 수정

--- src/main/java/com/api/apiserver/DTO/ordersitem/OrdersItemDTO.java
+++ src/main/java/com/api/apiserver/DTO/ordersitem/OrdersItemDTO.java
@@ -42,7 +42,6 @@ public class OrdersItemDTO {
                 .price(ordersItem.getPrice())
                 .qty(ordersItem.getQty())
                 .totalPrice(ordersItem.getPrice() * ordersItem.getQty())
-                .totalPrice(ordersItem.getPrice() * ordersItem.getQty())
                 .shipped(ordersItem.getShipped())
                 .canceled(ordersItem.getCanceled())
                 .picked(ordersItem.getPicked())
